734e7fcd362dcaab3fdabfdbbaa6c17d85d18d7d
Ayush <ayushpanchal005@gmail.com>
2024-04-09 17:52:54 +0530

    added the utility function to send verification mails and otp

--- models/OTP.js
+++ models/OTP.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
-const otpSchema = new mongoose.Schema({
+const OTPSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -16,4 +17,24 @@ const otpSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('OTP', otpSchema);
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      'Verification Email from AWT Project',
+      otp
+    );
+    console.log('Email sent successfully', mailResponse);
+  } catch (err) {
+    console.error('Error occured while sending mail: ', err);
+    throw err;
+  }
+}
+
+// using pre save middleware, ie, the following code should run before the document is saved
+OTPSchema.pre('save', async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+});
+
+module.exports = mongoose.model('OTP', OTPSchema);
