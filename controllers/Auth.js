2ecc7472fb7a3686afe31a276a5582e04b1d7903
Ayush <ayushpanchal005@gmail.com>
2024-04-10 10:38:03 +0530

    added the login controller

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,9 @@ const User = require('../models/User');
 const OTP = require('../models/OTP');
 const Profile = require('../models/Profile');
 const otpGenerator = require('otp-generator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 // sendOTP
 exports.sendOTP = async (req, res) => {
@@ -159,5 +162,76 @@ exports.signUp = async (req, res) => {
 };
 
 // login
+exports.login = async (req, res) => {
+  try {
+    // get data from req body
+    const { email, password } = req.body;
+
+    // data validation
+    if (!email || !password) {
+      return res.status(403).json({
+        success: false,
+        message: 'Both email and password are required. Please try again',
+      });
+    }
+
+    // check user exists or not
+    const user = await User.findOne({ email }).populate('additionalDetails');
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'User is not registered. Please sign up first.',
+      });
+    }
+
+    // generate JWT, after password matching
+    if (await bcrypt.compare(password, user.password)) {
+      const payload = {
+        email: user.email,
+        id: user._id,
+        role: user.role,
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: '2h',
+      });
+
+      user.token = token;
+      user.password = undefined;
+
+      // create cookie and send response
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.cookie('token', token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: 'Logged in successfully',
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: 'Incorrect Password ',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: 'User cannot be logged in. Please try again',
+    });
+  }
+};
 
 // changePassword
+exports.changePassword = async (req, res) => {
+  // get data from req body
+  // get oldpwd, newpwd, confirmNewPwd
+  // validation
+  // update pwd in DB
+  // send mail - pwd updated
+  // return response
+};
